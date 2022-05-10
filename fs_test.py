7672a9471f6eecc4847771564ecb94ad5c15bb73
neverCV <92416409+neverCV@users.noreply.github.com>
2022-05-10 14:47:07 +0800

    Update fs_test.py

--- fs_test.py
+++ fs_test.py
@@ -4,7 +4,7 @@ import paddle
 import os
 
 n_way, n_support, n_query = 5, 1, 16
-num_workers, numepochs = 0, 3
+num_workers, numepochs = 0, 1
 encoder_statedict = paddle.load(os.path.join('./save', 'baselinepp_55_model'))
 testdataset_1shot = Mini_Imagenet.metaDataSet('test',imagepath = './images')
 testdatasetsamplerr_1shot = Mini_Imagenet.BatchSampler('test', n_way, n_support, n_query, len=600, ep_per_batch=1)
@@ -15,7 +15,7 @@ model_zoo.baselinepp_test(test_loader_1shot, encoder_statedict, numepochs, n_way
 
 testdataset_5shot = Mini_Imagenet.metaDataSet('test',imagepath = './images')
 testdatasetsamplerr_1shot = Mini_Imagenet.BatchSampler('test', n_way, 5, n_query, len=600, ep_per_batch=1)
-test_loader_1shot = paddle.io.DataLoader(testdataset_1shot, batch_sampler=testdatasetsamplerr_1shot,
+test_loader_5shot = paddle.io.DataLoader(testdataset_5shot, batch_sampler=testdatasetsamplerr_1shot,
                                          num_workers=num_workers, use_shared_memory=True)
-model_zoo.baselinepp_test(test_loader_1shot, encoder_statedict, numepochs, n_way=n_way, n_support=5,
+model_zoo.baselinepp_test(test_loader_5shot, encoder_statedict, numepochs, n_way=n_way, n_support=5,
                                     n_query=n_query)
