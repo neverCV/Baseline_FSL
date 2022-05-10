1946080082af7ce908269dfcfafdde9197cba300
neverCV <92416409+neverCV@users.noreply.github.com>
2022-05-10 13:14:44 +0800

    Update fs_train.py

--- fs_train.py
+++ fs_train.py
@@ -4,7 +4,7 @@ import Mini_Imagenet
 import paddle.optimizer as pdoptimizer
 
 model = model_zoo.get_baselinepp_model()
-pretraindataset = Mini_Imagenet.classifyDataSet(imagepath = '../images')
+pretraindataset = Mini_Imagenet.classifyDataSet(imagepath = './images')
 base_loader = paddle.io.DataLoader(pretraindataset, batch_size=16, shuffle=True, num_workers=14)
 optimizer = pdoptimizer.Adam(parameters=model.parameters(), learning_rate=0.001)
 model = model_zoo.baselinepp_train(base_loader, model, optimizer, start_epoch=0, stop_epoch=50, save_freq=5)
